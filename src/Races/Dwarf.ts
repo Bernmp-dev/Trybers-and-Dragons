41443f684c22ce754d21bd40d5fdd0681483de9f
eon <bernardomp.dev@gmail.com>
2023-06-12 19:52:02 -0300

    feat: criadas todas races

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Dwarf extends Race {
+  private _maxLifePoints: number;
+  private static _orcInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 80;
+    Dwarf._orcInstances += 1;
+  }
+
+  public override get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    return Dwarf._orcInstances;
+  }
+}
+
+export default Dwarf;
