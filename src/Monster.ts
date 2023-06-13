22eecef2e1638b6e22f0e0375bd2690ebf1184d6
eon <bernardomp.dev@gmail.com>
2023-06-13 17:22:29 -0300

    feat: class Monster.

---
+++ src/Monster.ts
@@ -0,0 +1,41 @@
+import { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints >= this._lifePoints) {
+      this._lifePoints = -1;
+    }
+
+    if (attackPoints > 0) {
+      this._lifePoints -= attackPoints;
+    }
+
+    if (attackPoints <= 0) {
+      this._lifePoints -= 1;
+    }
+
+    return this._lifePoints;
+  }
+}
+
+export default Monster;
